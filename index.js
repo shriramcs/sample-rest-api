3993796a43d6d5d4bbc651a0a76955cd17c96986
shriramcs <shriramcs@gmail.com>
2020-07-01 12:02:06 +0530

    added html content

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const port  = 3000;
+
+app.get('/api/offers', (req, res) => {
+    const resp = [
+        {
+            id: 1,
+            data: 'sdaffds'
+        }, {
+            id: 1,
+            data: 'sdaffds'
+        }, {
+            id: 1,
+            data: 'sdaffds'
+        }
+    ]
+    res.json(resp);
+});
+
+app.get('/api/getSyllabus', function(req, res) {
+    var contents = fs.readFileSync("syllabus.json");
+    res.send(JSON.parse(contents));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/book', (req, res) => {
+    // We will be coding here
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
